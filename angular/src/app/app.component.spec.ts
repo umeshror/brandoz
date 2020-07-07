10996a400865a777e65ebddb13e8c9714954854f
umeshror <sarukumesh@gmail.com>
2020-07-08 00:53:59 +0530

    added HttpClientTestingModule

--- angular/src/app/app.component.spec.ts
+++ angular/src/app/app.component.spec.ts
@@ -28,10 +28,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('angular-app');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-app app is running!');
-  });
 });
